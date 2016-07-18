70256ef04d8f8159745eabb0dc81582667289ef3
Alexander <sanych.zp@gmail.com>
2016-07-18 18:52:43 +0300

    Update isValidColor.php

--- isValidColor.php
+++ isValidColor.php
@@ -2,16 +2,27 @@
 
 declare(strict_types = 1);
 
+
+/**
+ * CSS color validation.
+ * Returns true, if passed string is correct CSS color.
+ *
+ * @param string $color Color string
+ *
+ * @return bool
+ */
 function isValidColor(string $color) : bool
 {
-	$color = str_replace(["\f", "\n", "\r", "\t", "\v", ' '], '', strtolower($color));
-	$textColors = [
-	    'transparent', 'white', 'silver', 'gray', 'black', 'maroon', 'red', 'orange', 'yellow',
+    $color = str_replace(["\f", "\n", "\r", "\t", "\v", ' '], '', strtolower($color));
+    $textColors = [
+        'transparent', 'white', 'silver', 'gray', 'black', 'maroon', 'red', 'orange', 'yellow',
         'olive', 'lime', 'green', 'aqua', 'blue', 'navy', 'teal', 'fuchsia', 'purple'
     ];
-		
-	return in_array($color, $textColors, true) ?: (bool) preg_match('#^(?:\#[\da-f]{3}|[\da-f]{6})|(?:rgb(a)?\((?:(?:1?\d{1,2}|2(5)?(?(2)[0-5]|[0-4]\d)),){2}(?:1?\d{1,2}|2(5)?(?(3)[0-5]|[0-4]\d))(?(1),(?:1(?:\.0+)?|0?\.\d+))\))|(?:hsl(a)?\((?:[1-2]?\d{1,2}|3[0-5]\d),1?\d{1,2}%,1?\d{1,2}%(?(4),(?:1(?:\.0+)?|0?\.\d+))\))$#i', $color);
+
+    return in_array($color, $textColors, true) ?: (bool) preg_match('#^(?:\#[\da-f]{3}|[\da-f]{6})|(?:rgb(a)?\((?:(?:1?\d{1,2}|2(5)?(?(2)[0-5]|[0-4]\d)),){2}(?:1?\d{1,2}|2(5)?(?(3)[0-5]|[0-4]\d))(?(1),(?:1(?:\.0+)?|0?\.\d+))\))|(?:hsl(a)?\((?:[1-2]?\d{1,2}|3[0-5]\d),1?\d{1,2}%,1?\d{1,2}%(?(4),(?:1(?:\.0+)?|0?\.\d+))\))$#i', $color);
 }
+
+
 
 var_dump(
     isValidColor('   white   '), // true
